c875e2c721d475c20be327699229249453ecf6be
markojakovljevic <mjakovljevic49@gmail.com>
2023-03-17 12:10:10 +0100

    footer style

--- src/Packages/client/Home/Home.js
+++ src/Packages/client/Home/Home.js
@@ -11,7 +11,6 @@ const Home = () => {
     <span className='multiText_2'>FITNESS MANAGEMENT</span>
     </p>
       <video style= {{width:"100%", height: "100%"}}  className='video' autoPlay loop muted  src="/Pictures/Gym Example Compressed mp4.mp4">
-        
       </video>
     </div>
     </div>
