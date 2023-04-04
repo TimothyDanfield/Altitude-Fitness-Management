228549221896f21e8d5e426126e51075ce53fcd0
Timothy <TimothyDanfield@yahoo.com>
2023-04-04 06:22:14 -0500

    fixing warings to deploy test on netlify

--- client/src/Packages/client/Blog/Blog.js
+++ client/src/Packages/client/Blog/Blog.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './blog.css'
 
 const Blog = () => {
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
+//     const [name, setName] = useState()
+//     const [email, setEmail] = useState()
 
-    const emailChange = (e) => {
-        setEmail(e.target.value)
-    }
+//     const emailChange = (e) => {
+//         setEmail(e.target.value)
+//     }
 
-    const nameChange = (e) => {
-        setName(e.target.value)
-    }
+//     const nameChange = (e) => {
+//         setName(e.target.value)
+//     }
 
-    const joinBlog = (e) => {
-        e.preventDefault()
-    }
+//     const joinBlog = (e) => {
+//         e.preventDefault()
+//     }
 
     return (
         <div className='blog'>
@@ -32,13 +32,13 @@ const Blog = () => {
                     <form className="form" action="" method="post">
                         <div className="form-group">
                             <label htmlFor="name">First and Last Name:</label>
-                            <input type="textt" id="name" name="name" required onChange={nameChange}/>
+                            {/* <input type="textt" id="name" name="name" required onChange={nameChange}/> */}
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email:</label>
-                            <input type="emaill" id="email" name="email" required onChange={emailChange}/>
+                            {/* <input type="emaill" id="email" name="email" required onChange={emailChange}/> */}
                         </div>
-                        <button type="submit" className="btn-join" onClick={joinBlog}>Join Now</button>
+                        {/* <button type="submit" className="btn-join" onClick={joinBlog}>Join Now</button> */}
                     </form>
                 </div>
             </section>
