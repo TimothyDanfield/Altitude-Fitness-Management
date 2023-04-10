34ebb5d3471957dc92b198aaa71055a317ab9e7c
Timothy <TimothyDanfield@yahoo.com>
2023-04-10 17:41:21 -0500

    pulling

--- client/src/Packages/client/Services/FitTechSolutions.js
+++ client/src/Packages/client/Services/FitTechSolutions.js
@@ -1,44 +1,47 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './fittechsolutions.css'
 
 function Automation() {
   return (
-    <div className="container">
-      <div className="row align-items-center justify-content-center">
-      <div className="col-md-6 fitImg" style={{marginTop: '20px', width: '100%'}}>
-          <img src="/Pictures/fittech.png" alt="" style={{width: '100%'}}/>
-        </div>
-        <div className="col-md-6">
-          <h1 className="mb-4">
-            <span style={{ borderBottom: '1px solid black' }}>Fit</span>
-            <span className="text-primary">Tech Solutions</span>
-          </h1>
-          <p className="text-muted lead mb-5">
-            Our Fit Tech Solutions feature cutting-edge software in fitness technology which function to ensure that your gym is not wasting any time pushing papers. With features that can automate billing, scheduling, member management, and more, our gym management software can save you time and money while improving the overall efficiency of your operations.
-          </p>
-          <h2 className="mb-3 text-primary">Our automation services include:</h2>
-          <ol className="mb-5">
-            <li>
-              <p>Automated billing: Our gym management software allows you to automate your billing and payment processes, ensuring that you get paid on time and eliminating the need for manual invoicing and tracking.</p>
-            </li>
-            <li>
-              <p>Automated scheduling: Through Altitude Fitness software, we can help you streamline your scheduling process by automating class and appointment bookings, sending reminders to staff and clients, and managing waitlists and cancellations.</p>
-            </li>
-            <li>
-              <p>Automated member management: Our software can help you manage your member database, including signups, renewals, and cancellations. You can also track member attendance and preferences, and send personalized communications to members based on their interests and needs.</p>
-            </li>
-            <li>
-              <p>Automated reporting: Not sure what in your gym is clicking? Our fitness software can generate customized reports on key metrics such as revenue, attendance, and retention, helping you to make informed decisions and monitor your progress towards your goals.</p>
-            </li>
-          </ol>
-          <p className="text-muted lead mb-5">
-            Our gym management software is user-friendly, cloud-based, and customizable to meet the specific needs of your gym. We also provide training and support to ensure that you and your staff are fully equipped to use the software effectively.
-          </p>
-          <p className="text-muted lead mb-5">
-            By automating routine tasks and processes, our automation services can help you save time and focus on what really matters – providing a great experience for your members and growing your gym. Contact us to learn more about how we can help you streamline your operations and achieve your goals.
-          </p>
-        </div>
+    <div className="block">
+      <div className="fitImg">
+      <img  src="/Pictures/fittech.png" alt="" />
       </div>
+      <h1 className='fit-design-header' ><span style={{borderBottom: '1px solid black'}}>Fit</span><span>Tech Solutions</span></h1>
+      <p className="fit-design-body"><b>Our Fit Tech Solutions feature cutting-edge software in fitness technology which function to ensure that your gym is not wasting any time pushing papers. With features that can automate billing, scheduling, member management, and more, our gym management software can save you time and money while improving the overall efficiency of your operations. </b></p>
+      <br />
+      <br />
+      <p className="text">Our automation services include:</p>
+      <br />
+      <br />
+      <ol>
+        <li>
+          <h2 className="fit-design-includes-heading">Automated billing: 
+          <p className='automatedtext'>Our gym management software allows you to automate your billing and payment processes, ensuring that you get paid on time and eliminating the need for manual invoicing and tracking. </p>
+          </h2>
+        </li>
+        <li>
+          <h2 className="fit-design-includes-heading">Automated scheduling:
+          <p className='automated text1'>Through Altitude Fitness software, we can help you streamline your scheduling process by automating class and appointment bookings, sending reminders to staff and clients, and managing waitlists and cancellations. </p> 
+           </h2>
+       
+        </li>
+        <li>
+          <h2 className="fit-design-includes-heading">Automated member management:
+          <p className="text"> Our software can help you manage your member database, including signups, renewals, and cancellations. You can also track member attendance and preferences, and send personalized communications to members based on their interests and needs. </p>
+          </h2>
+        </li>
+        <li>
+          <p className="text">Automated reporting: Not sure what in your gym is clicking? Our fitness software can generate customized reports on key metrics such as revenue, attendance, and retention, helping you to make informed decisions and monitor your progress towards your goals. </p>
+        </li>
+      </ol>
+      <br />
+      <br />
+      <p className="text">Our gym management software is user-friendly, cloud-based, and customizable to meet the specific needs of your gym. We also provide training and support to ensure that you and your staff are fully equipped to use the software effectively. </p>
+      <br />
+      <br />
+      <br />
+      <p className="text">By automating routine tasks and processes, our automation services can help you save time and focus on what really matters – providing a great experience for your members and growing your gym. Contact us to learn more about how we can help you streamline your operations and achieve your goals. </p>
     </div>
   );
 }
