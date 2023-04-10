355b22d3648a5fb4134d308b06824e84d0d1ba18
Timothy <TimothyDanfield@yahoo.com>
2023-04-10 08:14:41 -0500

    Style changes to Services

--- client/src/Packages/client/Services/GymLaunch.js
+++ client/src/Packages/client/Services/GymLaunch.js
@@ -1,41 +1,48 @@
 import React from 'react';
 
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
 function GymLaunch() {
   return (
     <div className="block">
-      <div className="gymImg">
-        <img src="/pictures/gym.photo.png" alt="" />
-      </div>
-      <h1 ><span style={{borderBottom: '1px solid black'}}>Gym</span><span>Launch Facility Consultation</span></h1>
-      
+      <Container>
+        <Row>
+          <Col md={6} className="gymImg">
+            <Image src="/Pictures/design2.png" fluid />
+          </Col>
+          <Col md={6}>
+            <h1><span style={{ borderBottom: '1px solid black' }}>Gym</span><span>Launch Facility Consultation</span></h1>
+            <p className="text">
+              <b>
+                It's no secret that opening a gym can be difficult. Our consultation services are designed to help you navigate the tricky process and make informed decisions about every aspect of your facility. Our experienced consultants will work with you to develop a customized plan that takes into account your budget, goals, and timeline.
+              </b>
+              <br />
+              <br />
 
-      <p className="text">
-        <b>
-          It's no secret that opening a gym can be difficult. Our consultation services are designed to help you navigate the tricky process and make informed decisions about every aspect of your facility. Our experienced consultants will work with you to develop a customized plan that takes into account your budget, goals, and timeline.
-        </b>
-        <br />
-        <br />
+              Facility Design and Layout - The design and layout of your facility play a crucial role in attracting and retaining members. Our facility design and layout services are designed to help you create a space that is functional, attractive, and optimized for maximum revenue. We work with clients to identify your target demographics, evaluate your space, and develop a design that meets your needs and preferences.
+              <br />
+              <br />
 
-        Facility Design and Layout - The design and layout of your facility play a crucial role in attracting and retaining members. Our facility design and layout services are designed to help you create a space that is functional, attractive, and optimized for maximum revenue. We work with clients to identify your target demographics, evaluate your space, and develop a design that meets your needs and preferences.
-        <br />
-        <br />
+              Equipment Selection and Installation - Choosing the right equipment is essential to the success of your fitness facility. Our equipment selection and installation services are designed to help you choose the right equipment for your space, budget, and target market. We work with top equipment manufacturers to provide you with high-quality equipment that is reliable and durable.
+              <br />
+              <br />
 
-        Equipment Selection and Installation - Choosing the right equipment is essential to the success of your fitness facility. Our equipment selection and installation services are designed to help you choose the right equipment for your space, budget, and target market. We work with top equipment manufacturers to provide you with high-quality equipment that is reliable and durable.
-        <br />
-        <br />
+              Marketing and Advertising - Need to fill your pristine new gym? Our marketing and advertising services are designed to help you create a strong brand identity and reach your target market. We offer a range of services, including social media management, search engine optimization, pay-per-click advertising, and more.
+              <br />
+              <br />
 
-        Marketing and Advertising - Need to fill your pristine new gym? Our marketing and advertising services are designed to help you create a strong brand identity and reach your target market. We offer a range of services, including social media management, search engine optimization, pay-per-click advertising, and more.
-        <br />
-        <br />
+              Staffing and Training - Hiring and training the right staff is critical to the success of your fitness facility. Our staffing and training services are designed to help you find and develop top talent. We offer recruitment services, employee training and development programs, and employee retention programs to help you build a strong and stable team.
+              <br />
+              <br />
 
-        Staffing and Training - Hiring and training the right staff is critical to the success of your fitness facility. Our staffing and training services are designed to help you find and develop top talent. We offer recruitment services, employee training and development programs, and employee retention programs to help you build a strong and stable team.
-        <br />
-        <br />
-
-        At Altitude Fitness Management Group, we have the expertise and experience to help you open a successful fitness facility. Contact us to learn more about our consultation and other services and how we can help you achieve your goals.
-      </p>
+              At Altitude Fitness Management Group, we have the expertise and experience to help you open a successful fitness facility. Contact us to learn more about our consultation and other services and how we can help you achieve your goals.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default GymLaunch;
+
--- client/src/Packages/client/Services/StaffAugmentation.js
+++ client/src/Packages/client/Services/StaffAugmentation.js
@@ -1,49 +1,63 @@
 import React from 'react';
-import './staffaugmentation.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Employee() {
   return (
     <div className="card-container-staff">
-      <div className="staff-augmentation-header-container">
-        <img src="/Pictures/staffaugmentation.jpg" alt="" />
+      <div className="col-md-6 staffImg" style={{marginTop: '20px', width: '80%'}}>
+        <img src="/Pictures/staffaugmentation.jpg" alt="" className="img-fluid" style={{width: '100%'}} />
       </div>
-      <div className='test'>
-      <div className='staff-header-content'>
-      <h1 className="staff-header" ><span style={{borderBottom: '1px solid black'}}>Sta</span><span>ff Augmentation</span></h1>
-      </div>
-      <p className="staff-body">
-      <p>
-        <strong>
-          Struggling to staff your gym? We offer both on-demand employees to help with custodial and administrative duties, as well as a professional recruiter who can handle all your hiring needs.{' '}
-        </strong>
+      <div className='col-md-6'>
+        <div className='mb-4'>
+          <h1 className="mb-4" ><span style={{borderBottom: '1px solid black'}}>Sta</span><span>ff Augmentation</span></h1>
+        </div>
+        <div className="text-center">
+        <p className="staff-body">
+          <p className="font-weight-bold mb-0">
+            Struggling to staff your gym? We offer both on-demand employees to help with custodial and administrative duties, as well as a professional recruiter who can handle all your hiring needs.
+          </p>
+          <br />
+          <br />
+          <p className='staff-includes-heading font-weight-bold'>
+            Staff Augmentation includes:
+          </p>
+          <br />
+          <br />
+          <div class="row">
+  <div class="col-md-12">
+    <ol class="pl-3">
+      <li>
+        <span>On-demand employee services:</span>
+        <span>We understand that staffing a gym can be a challenge, especially when it comes to custodial and administrative duties. That's why we offer on-demand employee services to help with tasks such as cleaning, front desk support, and member check-ins. Our on-demand employees are trained, reliable, and available when you need them, allowing you to focus on your core business.</span>
+      </li>
+      <li>
+        <span>Professional recruitment services:</span>
+        <span>Finding the right people to join your team can be a time-consuming and costly process. That's why we offer professional recruitment services to help you find qualified candidates for your gym. Our team of experienced recruiters will work with you to understand your needs and preferences and will identify and screen candidates who meet your criteria. We can handle all aspects of the hiring process, from posting job listings to conducting interviews and negotiating offers.</span>
+      </li>
+      <li>
+        <span>Employee training and development:</span>
+        <span>We believe in investing in your employees to help them grow and develop professionally. That's why we offer customized training and development programs to help your staff improve their skills and performance. Our programs cover a range of topics, including sales and customer service, leadership development, and industry-specific training.</span>
+      </li>
+      <li>
+        <span>Employee retention programs:</span>
+        <span>Keeping your employees motivated and engaged is key to building a strong and stable team. That's why we offer employee retention programs that focus on creating a positive and supportive workplace culture. Our programs include team-building activities, recognition and reward programs, and ongoing feedback and coaching.</span>
+      </li>
+    </ol>
+  </div>
+</div>
+
+          <br />
+          <br />
+          <p className='staff-text'>
+            At Altitude Fitness Management Group, we believe that your employees are your most valuable asset. That's why we are committed to providing comprehensive employee services that help you attract, retain, and develop a high-performing team. Contact us to learn more about how we can help you achieve your employee management goals.
+          </p>
         </p>
-        <br />
-        <br />
-        <p className='staff-includes-heading'>
-        Staff Augmentation includes:
-        </p>
-        <br />
-        <br />
-        1. On-demand employee services: We understand that staffing a gym can be a challenge, especially when it comes to custodial and administrative duties. That's why we offer on-demand employee services to help with tasks such as cleaning, front desk support, and member check-ins. Our on-demand employees are trained, reliable, and available when you need them, allowing you to focus on your core business.
-        <br />
-        <br />
-        2. Professional recruitment services: Finding the right people to join your team can be a time-consuming and costly process. That's why we offer professional recruitment services to help you find qualified candidates for your gym. Our team of experienced recruiters will work with you to understand your needs and preferences and will identify and screen candidates who meet your criteria. We can handle all aspects of the hiring process, from posting job listings to conducting interviews and negotiating offers.
-        <br />
-        <br />
-        3. Employee training and development: We believe in investing in your employees to help them grow and develop professionally. That's why we offer customized training and development programs to help your staff improve their skills and performance. Our programs cover a range of topics, including sales and customer service, leadership development, and industry-specific training.
-        <br />
-        <br />
-        4. Employee retention programs: Keeping your employees motivated and engaged is key to building a strong and stable team. That's why we offer employee retention programs that focus on creating a positive and supportive workplace culture. Our programs include team-building activities, recognition and reward programs, and ongoing feedback and coaching.
-        <br />
-        <br />
-        <br />
-        <p className='staff-text'>
-        At Altitude Fitness Management Group, we believe that your employees are your most valuable asset. That's why we are committed to providing comprehensive employee services that help you attract, retain, and develop a high-performing team. Contact us to learn more about how we can help you achieve your employee management goals.
-        </p>
-      </p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Employee;
+
